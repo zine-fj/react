a962c88f9e5fa0abed2a358a9289383bcdcc4b1e
zine-fj <747810974@qq.com>
2018-06-26 08:22:27 +0800

    案例

--- props_state/js/list.js
+++ props_state/js/list.js
@@ -7,9 +7,7 @@ let Header = React.createClass({
                       <a href="javascript:window.history.go(-1);">{"<"}</a>
                   </li>
                   <li className="header-tit">{this.props.children}</li>
-                  <li className="header-btn">
-
-                  </li>
+                  <li className="header-btn"></li>
               </ul>
           </div>
       )
@@ -25,14 +23,18 @@ let Content = React.createClass({
 // 分类组件
 let ClassList = React.createClass({
   changeClass(id) {
+    // 通过id改变父组件的listData
     console.log(id)
+    this.props.getProData(id) // 调用父组件方法通过id获取数据
   },
   render() {
     return (
       <ul className="class-list">
         {
           this.props.classData.map((ele,index)=>{
-            return <li onClicd={this.changeClass.bind(this,ele.classID)} key={index}>{ele.className}</li>
+            return <li 
+            onClick={()=>this.changeClass(ele.classID)} 
+            key={index}>{ele.className}</li>
           })
         }
       </ul>
@@ -44,15 +46,13 @@ let ClassList = React.createClass({
 let ProList = React.createClass({
   render() {
     return (
-      <ul className="pro-list">
+      <ul listData="pro-list">
         {
           this.props.listData.map((ele,index)=>{
-            return (
-              <li key={index}>
-                <img src={ele.goodsListImg} alt=""/>
-                <p>{ele.goodsName}</p>
-              </li>
-            )
+            return <li key={index}>
+              <img src={ele.goodsListImg} alt=""/>
+              <p>{ele.goodsName}</p>
+            </li>
           })
         }
       </ul>
@@ -61,36 +61,44 @@ let ProList = React.createClass({
 })
 
 // 页面 顶层组件(管理数据)
-var ListPage = React.createClass({
+let ListPage = React.createClass({
     getInitialState() {
       return {
-        classData:[{className:"衬衫"}],
-        listData:[{goodsName:"休闲衬衫"}]
+        listData:[{goodsName:"休闲衬衫"}],
+        classData:[{className:"衬衫"}]
       }
     },
     render(){
       const {listData,classData} = this.state;
       return (
-        <div>
+        <div id=""list-page>
           <Header>商品列表</Header>
           {/* 把自己的数据传递给子组件 */}
-          <ClassList classData={classData}/>
+          <ClassList classData={classData} getProData={this.getProData} />
           <Content >
-            <ProList listData={listData}/>
+            <ProList listData={listData} />
           </Content>
         </div>
       )
     },
+    getProData(classID) {
+      // 可以根据classID获取对应分类数据
+      fetchJsonp("http://datainfo.duapp.com/shopdata/getGoods.php?classID="+classID).then(res=>res.json()).then(data=>{
+        this.setState({
+          listData:data
+        })
+      })
+    },
     componentDidMount() {
-      // 获取分类数据
+      // 获取分类数据 http://datainfo.duapp.com/shopdata/getclass.php
       fetch('http://datainfo.duapp.com/shopdata/getclass.php').then(res=>res.json()).then(data=>{
         this.setState({
           classData:data
         })
       })
 
-      // 获取商品数据 (需要使用jsonp，可以用fetch-jsonp插件)
-      fetchJsonp('http://datainfo.duapp.com/shopdata/getGoods.php?classID=2').then(res=>res.json()).then(data=>{
+      // 获取商品数据 (需要使用jsonp，可以用fetch-jsonp插件) http://datainfo.duapp.com/shopdata/getGoods.php?classID=2
+      fetchJsonp('http://datainfo.duapp.com/shopdata/getGoods.php?classID=1').then(res=>res.json()).then(data=>{
         this.setState({
           listData:data
         })
