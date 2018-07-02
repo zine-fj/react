64b1e862ba93ccacb54a0983a190811cc28f1f25
zine-fj <747810974@qq.com>
2018-07-02 11:49:33 +0800

    redux

---
+++ redux/src/index-basic.js
@@ -0,0 +1,46 @@
+import {createStore} from "redux"
+
+// 管理员
+const reducer = (state={price:15000},action)=>{
+    switch(action.type) {
+        case "borrow13000":
+            return {price:state.price-13000}
+        case "borrow15000":
+            console.log('想得美')
+            return state
+        default:
+        // 不能预测到的结果就不管
+            return state
+    }
+    
+}
+
+// 创建仓库
+const store = createStore(reducer)
+console.log(store.getState())
+
+let xiaoxin = {
+    subscribe(){
+        // 订阅
+        store.subscribe(function() {
+            // 如果钱有变动，就会触发
+            console.log('短信通知',store.getState())
+        })
+    }
+}
+xiaoxin.subscribe()
+
+// 行为：借13000
+const action = {type:'borrow15000'}
+
+// 发起请求
+setTimeout(function() {
+    store.dispatch(action)
+},2000)
+
+setTimeout(function() {
+    store.dispatch({type:'borrow13000'})
+},4000)
+
+// 再看看有多少
+console.log('再看看有多少',store.getState())
--- redux/src/index.js
+++ redux/src/index.js
@@ -1,46 +1,73 @@
 import {createStore} from "redux"
+import React,{Component} from "react"
+import {render} from "react-dom"
 
 // 管理员
-const reducer = (state={price:15000},action)=>{
+const reducer = function(state={price:20000},action) {
     switch(action.type) {
-        case "borrow13000":
-            return {price:state.price-13000}
-        case "borrow15000":
-            console.log('想得美')
-            return state
+        case "borrow":
+            console.log('borrow')
+            // 想要借钱的金额
+            let price = action.payload.price
+            if(price <= 3000) {
+                return {price:state.price-price} // 不要直接修改state
+            } else {
+                return state
+            }
         default:
-        // 不能预测到的结果就不管
             return state
     }
-    
 }
-
-// 创建仓库
+// 创建store
 const store = createStore(reducer)
-console.log(store.getState())
 
-let xiaoxin = {
-    sub(){
-        // 订阅
-        store.subscribe(function() {
-            // 如果钱有变动，就会触发
-            console.log('短信通知',store.getState())
+class Cont extends Component {
+    state={
+        price:store.getState().price // 从store里面获取初始的 price
+    }
+    render() {
+        return (
+            <p>{this.state.price}</p>
+        )
+    }
+    componentDidMount() {
+        // 对store里面的钱进行监控
+        store.subscribe(()=>{
+            // 如果金额发生变化，就会自动调用 subscribe
+            // 重新获取 store 里面的price
+            this.setState({
+                price:store.getState().price
+            })
         })
     }
 }
-xiaoxin.sub()
 
-// 行为：借13000
-const action = {type:'borrow15000'}
+class Button extends Component {
+    fnBorrow() {
+        // 借钱，派发请求
+        store.dispatch({type:'borrow',payload:{price:2000}})
+    }
+    render() {
+        return (
+            <a onClick={this.fnBorrow} href="javascript:;">{this.props.children}</a>
+        )
+    }
+}
 
-// 发起请求
-setTimeout(function() {
-    store.dispatch(action)
-},2000)
+class Box extends Component {
+    render() {
+        return (
+            <div>
+                <Cont></Cont>
+                <Button>借钱</Button>
+            </div>
+        )
+    }
+}
 
-setTimeout(function() {
-    store.dispatch({type:'borrow13000'})
-},4000)
 
-// 再看看有多少
-console.log('再看看有多少',store.getState())
+
+render(
+    <Box />,
+    document.getElementById('root')
+)
