7cd875147a9ae91fab968356dfd03e4ae2db32be
zine-fj <747810974@qq.com>
2018-07-18 22:42:34 +0800

    list

--- antd/src/routes/Product/List/index.js
+++ antd/src/routes/Product/List/index.js
@@ -2,18 +2,28 @@ import React,{Component} from 'react'
 import {Link} from 'react-router'
 import {connect} from 'react-redux'
 import {NavBar} from 'antd-mobile'
+import './list.scss'
 
 class List extends Component{
     render() {
-        const {listData} = this.props
+        const {listData,activeIndex} = this.props
         return (
             <div id='list-page'>
                 <NavBar>列表</NavBar>
                 <div className='sub-header'>
-                    <a href="javascript:;">综合</a>
-                    <a href="javascript:;">销量</a>
-                    <a href="javascript:;" className='price'>价格</a>
-                    <a href="javascript:;">筛选</a>
+                    <a href="javascript:;" 
+                    onClick={()=>this.changeSort(0)} 
+                    className={activeIndex==0?'active':''}
+                    >综合</a>
+                    <a href="javascript:;" 
+                    onClick={()=>this.changeSort(1)} 
+                    className={activeIndex==1?'active':''}
+                    >销量</a>
+                    <a href="javascript:;" 
+                    onClick={()=>this.changeSort(2)}  
+                    className={activeIndex==2?'selected price':'price'}
+                    >价格</a>
+                    {/* <a href="javascript:;">筛选</a> */}
                 </div>
                 <div className='content'>
                     <ul className='product-list'>
@@ -23,6 +33,8 @@ class List extends Component{
                                     <Link to={'detail/'+ele.goodsId}>
                                         <img src={'http://image.loho88.com/'+ele.img} alt=""/>
                                         <p>{ele.title}</p>
+                                        <p>销量：{ele.salesNum}</p>
+                                        <p>价格：{ele.price}</p>
                                     </Link>
                                 </li>
                             })
@@ -32,11 +44,42 @@ class List extends Component{
             </div>
         )
     }
+    getListData(sortKey) {
+        fetch('loho88/search/?e=222&page=1&sort='+sortKey).then(res=>res.json()).then(data=>{
+            console.log(data)
+            this.props.dispatch({
+                type: "PRO_LIST_DATA",
+                payload: {
+                    listData: data.result.data
+                }
+            })
+        })
+    }
+    changeSort(index) {
+        this.props.dispatch({
+            type: "PRO_CHANGE_SORT",
+            payload: {
+                index
+            }
+        })
+    }
+    componentDidMount() {
+        // 获取初始数据
+        this.getListData(this.props.sortKey)
+    }
+    componentWillReceiveProps(newProps) {
+        if(newProps.sortKey != this.props.sortKey) {
+            // sortKey 改变
+            this.getListData(newProps.sortKey)
+        }
+    }
 }
 
 const mapStateToProps = state=>{
     return {
-        listData:state.productReducer.listData
+        listData:state.productReducer.listData,
+        activeIndex : state.productReducer.activeIndex,
+        sortKey : state.productReducer.sortKey
     }
 }
 
--- antd/src/store/reducers/product.js
+++ antd/src/store/reducers/product.js
@@ -1,7 +1,33 @@
-const reducer = (state={listData:[{title:"商品1"},{title:"商品2"}]},action) => {
+
+const initState = {listData:[],activeIndex:0,sortKey:''}
+const reducer = (state=initState,action) => {
     switch(action.type) {
-        case 'ss':
-            return state
+        case 'PRO_LIST_DATA':
+            var newState = JSON.parse(JSON.stringify(state)) // 拷贝state
+            newState.listData = action.payload.listData
+            return newState
+        case 'PRO_CHANGE_SORT':
+            var newState = Object.assign({},state) // 改变简单类型，浅拷贝也可以
+            const index = action.payload.index
+            var sortKey = state.sortKey // sortKey初始值
+
+            // 通过index判断，生成新的 sortKey
+            if(index === 1) {
+                sortKey = 'o1'
+            } else if (index === 0) {
+                sortKey = ''
+            } else if (index === 2) {
+                if(state.activeIndex === 2) { //切换升序降序
+                    sortKey = sortKey === 'o5' ? 'o3' : 'o5'
+                } else {
+                    sortKey = 'o5'
+                }
+                console.log(sortKey)
+            }
+            // 赋值给newState
+            newState.activeIndex = index
+            newState.sortKey = sortKey
+            return newState
         default:
             return state
     }
